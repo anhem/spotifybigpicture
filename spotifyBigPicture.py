4a2993d671a16958de79edb0fe93194808c04e45
Andreas Hemmingsson <andhem@gmail.com>
2013-10-26 00:03:21 +0200

    expanded modifier to include views xml files

--- spotifyBigPicture.py
+++ spotifyBigPicture.py
@@ -6,8 +6,13 @@ import re
 import os
 import argparse
 
-FILEPATH_OSX = '/Applications/Spotify.app/Contents/Resources/skin.xml'
-FILEPATH_LINUX = '/opt/spotify/spotify-client/Data/resources.zip'
+WIN = 'win'
+OSX = 'darwin'
+LINUX = 'linux'
+RESOURCES = 'Resources'
+RESOURCES_ZIP = 'resources.zip'
+RESOURCES_PATH_OSX = '/Applications/Spotify.app/Contents/' + RESOURCES
+RESOURCES_PATH_LINUX = '/opt/spotify/spotify-client/Data/' + RESOURCES_ZIP
 DEFAULT_FONT_SIZE = 8
 PATTERN = 'size="([^"]*)"'
 
@@ -18,27 +23,27 @@ parser.add_argument('-s', '--size', type=int, help='The size fonts should be cha
 parser.add_argument('-p', '--path', help='Specify path to Spotify resources.zip/skin.xml if not detected automatically')
 args = parser.parse_args()
 
-def backupFile(filePath):
-    backupPath = filePath + '.bak'
-    print 'Creating backup of %s to %s' % (filePath, backupPath)
+def backupResources(resourcesPath):
+    backupPath = resourcesPath + '.bak'
+    print 'Creating backup of %s to %s' % (resourcesPath, backupPath)
     if os.path.exists(backupPath):
         print 'backup already exists'
     else:
-        shutil.copy2(filePath, backupPath)
+        shutil.copy2(resourcesPath, backupPath)
 
-def restoreBackup(filePath):
-    backupPath = filePath + '.bak'
+def restoreResources(resourcesPath):
+    backupPath = resourcesPath + '.bak'
     print 'restoring default font size from backup %s' % backupPath
     if os.path.exists(backupPath):
-        os.remove(filePath)
-        os.rename(backupPath, filePath)
+        os.remove(resourcesPath)
+        os.rename(backupPath, resourcesPath)
     else:
         print 'No backup file found, restore not possible'
 
-def extractArchive(filePath):
+def extractArchive(resourcesPath):
     extractDir = tempfile.mkdtemp()
-    print 'extracting %s to %s' % (filePath, extractDir)
-    zf = zipfile.ZipFile(filePath, 'r')
+    print 'extracting %s to %s' % (resourcesPath, extractDir)
+    zf = zipfile.ZipFile(resourcesPath, 'r')
     try:
         for name in zf.namelist():
             zf.extract(name, extractDir)
@@ -46,9 +51,9 @@ def extractArchive(filePath):
         zf.close()
     return extractDir
 
-def compressArchive(srcDir, destFile):
-    print 'compressing files from %s to %s' % (srcDir, destFile)
-    zf = zipfile.ZipFile(destFile, mode='w')
+def compressArchive(srcDir, resourcesPath):
+    print 'compressing files from %s to %s' % (srcDir, resourcesPath)
+    zf = zipfile.ZipFile(resourcesPath, mode='w')
     try:
         for root, dirs, files in os.walk(srcDir):
             for filename in files:
@@ -58,23 +63,33 @@ def compressArchive(srcDir, destFile):
     finally:
         zf.close()
 
-def modifySkin(skinFile, fontSize):
-    print 'updating %s with font size %s' % (skinFile, fontSize)
-    newSkinFile = skinFile + '.new'
-    reg = re.compile(PATTERN)
-    with open(skinFile, 'r') as infile:
-        with open(newSkinFile, 'w') as outfile:
-            for line in infile:
-                value = reg.search(line)
-                if value is not None:
-                    oldSize = value.group(1)
-                    newSize = int(oldSize) + fontSize
-                    line = re.sub(reg, 'size="' + str(newSize) + '"', line)
-                outfile.write(line)
-    os.remove(skinFile)
-    os.rename(newSkinFile, skinFile)
+def getXmlFiles(resourcesPath):
+    xmlFiles = list()
+    xmlFiles.append(resourcesPath + '/skin.xml')
+    for root, dirs, files in os.walk(resourcesPath + '/views'):
+        for f in files:
+            xmlFiles.append(os.path.join(root, f))
+    return xmlFiles
 
-def getResourcePathForWindows():
+def modifyXmlFiles(resourcesPath, fontSize):
+    fileList = getXmlFiles(resourcesPath)
+    for xmlFile in fileList:
+        print 'updating %s with font size %s' % (xmlFile, fontSize)
+        newXmlFile = xmlFile + '.new'
+        reg = re.compile(PATTERN)
+        with open(xmlFile, 'r') as infile:
+            with open(newXmlFile, 'w') as outfile:
+                for line in infile:
+                    value = reg.search(line)
+                    if value is not None:
+                        oldSize = value.group(1)
+                        newSize = int(oldSize) + fontSize
+                        line = re.sub(reg, 'size="' + str(newSize) + '"', line)
+                    outfile.write(line)
+        os.remove(xmlFile)
+        os.rename(newXmlFile, xmlFile)
+
+def getResourcesPathForWindows():
     import _winreg
     key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, "Software\\Spotify")
     value = _winreg.QueryValueEx(key, "")[0]
@@ -82,17 +97,17 @@ def getResourcePathForWindows():
 
 if args.restore:
     if args.path:
-        if args.path.endswith('resources.zip') or args.path.endswith('skin.xml'):
-            restoreBackup(args.path)
+        if args.path.endswith(RESOURCES_ZIP) or args.path.endswith(RESOURCES):
+            restoreResources(args.path)
         else:
-            print 'File not recognized, must be either resources.zip or skin.xml'
+            print 'File not recognized, must be either %s or%s' % (RESOURCES_ZIP, RESOURCES)
             exit(0)
-    elif sys.platform.startswith('linux'):
-        restoreBackup(FILEPATH_LINUX)
-    elif sys.platform == 'darwin':
-        restoreBackup(FILEPATH_OSX)
-    elif sys.platform.startswith('win'):
-        restoreBackup(getResourcePathForWindows())
+    elif sys.platform.startswith(LINUX):
+        restoreResources(RESOURCES_PATH_LINUX)
+    elif sys.platform == OSX:
+        restoreResources(RESOURCES_PATH_OSX)
+    elif sys.platform.startswith(WIN):
+        restoreResources(getResourcesPathForWindows())
     else:
         print 'OS not recognized, use --path <file path> to manually specify the resources.zip or skin.xml'
         exit(0)
@@ -102,31 +117,31 @@ else:
         fontSize = args.size
 
     if args.path:
-        if args.path.endswith('resources.zip'):
-            backupFile(args.path)
+        if args.path.endswith(RESOURCES_ZIP):
+            backupResources(args.path)
             extractedDir = extractArchive(args.path)
-            modifySkin(extractedDir + '/' + 'skin.xml', fontSize)
+            modifyXmlFiles(extractedDir, fontSize)
             compressArchive(extractedDir, args.path)
-        elif args.path.endswith('skin.xml'):
-            backupFile(args.path)
-            modifySkin(args.path, fontSize)
+        elif args.path.endswith(RESOURCES):
+            backupResources(args.path)
+            modifyXmlFiles(args.path, fontSize)
         else:
-            print 'File not recognized, must be either resources.zip or skin.xml'
+            print 'File not recognized, must be either %s or%s' % (RESOURCES_ZIP, RESOURCES)
             exit(0)
     else:
-        if sys.platform.startswith('linux'):
-            backupFile(FILEPATH_LINUX)
-            extractedDir = extractArchive(FILEPATH_LINUX)
-            modifySkin(extractedDir + '/' + 'skin.xml', fontSize)
-            compressArchive(extractedDir, FILEPATH_LINUX)
-        elif sys.platform == 'darwin':
-            backupFile(FILEPATH_OSX)
-            modifySkin(FILEPATH_OSX, fontSize)
-        elif sys.platform.startswith('win'):
-            filePath_windows = getResourcePathForWindows()
-            backupFile(filePath_windows)
+        if sys.platform.startswith(LINUX):
+            backupResources(RESOURCES_PATH_LINUX)
+            extractedDir = extractArchive(RESOURCES_PATH_LINUX)
+            modifyXmlFiles(extractedDir, fontSize)
+            compressArchive(extractedDir, RESOURCES_PATH_LINUX)
+        elif sys.platform == OSX:
+            backupResources(RESOURCES_PATH_OSX)
+            modifyXmlFiles(RESOURCES_PATH_OSX, fontSize)
+        elif sys.platform.startswith(WIN):
+            filePath_windows = getResourcesPathForWindows()
+            backupResources(filePath_windows)
             extractedDir = extractArchive(filePath_windows)
-            modifySkin(extractedDir + '\\' + 'skin.xml', fontSize)
+            modifyXmlFiles(extractedDir, fontSize)
             compressArchive(extractedDir, filePath_windows)
         else:
             print 'OS not recognized, use --path <file path> to manually specify the resources.zip or skin.xml'
